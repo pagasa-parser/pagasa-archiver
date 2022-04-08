b0aa6f89e41f069ea37f3ddcd080dfc6b1ac57ce
ChlodAlejandro <chlod@chlod.net>
2022-04-09 07:52:03 +0800

    add scraper overrides

--- src/scraper/PagasaScraper.ts
+++ src/scraper/PagasaScraper.ts
@@ -9,8 +9,10 @@ export interface PAGASADocument {
 
 export default class PagasaScraper {
 
-    static readonly INDEX_URL_TCA = "https://pubfiles.pagasa.dost.gov.ph/tamss/weather/tca/";
-    static readonly INDEX_URL_TCB = "https://pubfiles.pagasa.dost.gov.ph/tamss/weather/bulletin/";
+    static readonly INDEX_URL_TCA =
+        process.env.TCA_INDEX_URL ?? "https://pubfiles.pagasa.dost.gov.ph/tamss/weather/tca/";
+    static readonly INDEX_URL_TCB =
+        process.env.TCB_INDEX_URL ?? "https://pubfiles.pagasa.dost.gov.ph/tamss/weather/bulletin/";
 
     static async findLinks(url: string, config: AxiosRequestConfig = {}) : Promise<string[]> {
         const { data } = (await axios(Object.assign(config, { responseType: "text", url: url })));
@@ -26,8 +28,10 @@ export default class PagasaScraper {
         return links;
     }
 
-    static async listTCAs(config: AxiosRequestConfig = {}) : Promise<PAGASADocument[]> {
-        return (await this.findLinks(this.INDEX_URL_TCA, config))
+    static async listTCAs(
+        config: AxiosRequestConfig & { indexURL?: string } = {}
+    ) : Promise<PAGASADocument[]> {
+        return (await this.findLinks(config.indexURL ?? this.INDEX_URL_TCA, config))
             .filter(v => /TCA%23(\d+)-?(F(?:INAL)?)?_(.+?)\.pdf/gi.test(v))
             .map(v => {
                 const file = decodeURIComponent(v);
@@ -41,8 +45,10 @@ export default class PagasaScraper {
             });
     }
 
-    static async listTCBs(config: AxiosRequestConfig = {}) : Promise<PAGASADocument[]> {
-        return (await this.findLinks(this.INDEX_URL_TCB, config))
+    static async listTCBs(
+        config: AxiosRequestConfig & { indexURL?: string } = {}
+    ) : Promise<PAGASADocument[]> {
+        return (await this.findLinks(config.indexURL ?? this.INDEX_URL_TCB, config))
             .filter(v => /TCB%23(\d+)-?(F(?:INAL)?)?_(.+?)\.pdf/gi.test(v))
             .map(v => {
                 const file = decodeURIComponent(v);
@@ -56,16 +62,20 @@ export default class PagasaScraper {
             });
     }
 
-    static async downloadTCA(tca: string, config: AxiosRequestConfig) : Promise<AxiosResponse> {
+    static async downloadTCA(
+        tca: string, config: AxiosRequestConfig & { indexURL?: string }
+    ) : Promise<AxiosResponse> {
         return axios(
-            new URL(encodeURIComponent(tca), this.INDEX_URL_TCA).toString(),
+            new URL(encodeURIComponent(tca), config.indexURL ?? this.INDEX_URL_TCA).toString(),
             config
         );
     }
 
-    static async downloadTCB(tcb: string, config: AxiosRequestConfig) : Promise<AxiosResponse> {
+    static async downloadTCB(
+        tcb: string, config: AxiosRequestConfig & { indexURL?: string }
+    ) : Promise<AxiosResponse> {
         return axios(
-            new URL(encodeURIComponent(tcb), this.INDEX_URL_TCB).toString(),
+            new URL(encodeURIComponent(tcb), config.indexURL ?? this.INDEX_URL_TCB).toString(),
             config
         );
     }
